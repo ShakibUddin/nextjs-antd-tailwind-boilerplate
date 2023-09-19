84eb98f6d50fbb1dfbe89e1598ae6e581d449514
ShakibUddin <shakibuddinbhuiyan@gmail.com>
2023-09-19 22:53:44 +0600

    updated login and signup page ui

--- pages/login/index.js
+++ pages/login/index.js
@@ -8,7 +8,7 @@ import InputField from "@/components/common/inputField";
 const Login = () => {
   return (
     <div className="w-full h-screen bg-[var(--brandColor)] flex justify-center items-center">
-      <div className="flex flex-col gap-4 justify-stretch p-4 m-4 rounded-md bg-white shadow-md w-full md:w-1/3 lg:w-1/4 ">
+      <div className="flex flex-col gap-4 justify-stretch p-4 m-4 rounded-md bg-white shadow-md w-full sm:w-2/3 md:w-2/3 lg:w-1/3">
         <span className="font-bold text-3xl text-[var(--brandColor)] text-center">
           COMPANY
         </span>
@@ -27,7 +27,7 @@ const Login = () => {
           }
           className="p-4"
         />
-        <Button className="bg-[var(--brandColor)] text-white mx-auto hover:text-[var(--brandColor)] hover:bg-white hover:border-2 hover:border-[var(--brandColor)]">
+        <Button className="bg-[var(--brandColor)] w-32 md:w-32 lg:w-48 h-8 md:h-8 lg:h-10  text-white mx-auto hover:text-[var(--brandColor)] hover:bg-white hover:border-2 hover:border-[var(--brandColor)]">
           LOGIN
         </Button>
         <p className="text-center">
