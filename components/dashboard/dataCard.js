6bb65613eae77c936f4a63f8dc48a256e4e8231e
ShakibUddin <shakibuddinbhuiyan@gmail.com>
2023-09-20 02:56:02 +0600

    added chart and dashboard cards

---
+++ components/dashboard/dataCard.js
@@ -0,0 +1,22 @@
+import Link from "next/link";
+import React from "react";
+
+const DataCard = ({ title, total, icon, bgColor }) => {
+  return (
+    <div
+      className={
+        "flex flex-col justify-center items-center p-4 w-64 h-48 rounded-md shadow-md " +
+        bgColor
+      }
+    >
+      {icon}
+      <p className="text-4xl font-bold text-center text-white">{total}</p>
+      <p className="text-sm font-bold text-center text-white">{title}</p>
+      <Link href={""} className="text-white text-sm mt-4 hover:text-white">
+        See More
+      </Link>
+    </div>
+  );
+};
+
+export default DataCard;
